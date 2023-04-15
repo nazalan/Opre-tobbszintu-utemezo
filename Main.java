a8accc5e286491837b6fbdc574478a655040c3f1
nazalan <zalan2552@gmail.com>
2023-04-15 23:19:20 +0200

    masodik

--- Main.java
+++ Main.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-//valtozas
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[][] data = new String[10][4];
+        int db=0;
+
+        for(int i = 0; i < 10; i++) {
+            String line = scanner.nextLine();
+            if(line.isEmpty()) break;
+            String[] parts = line.split(",");
+            for(int j = 0; j < 4; j++) {
+                data[i][j] = parts[j];
+            }
+            db++;
+        }
+        ArrayList<Task> tasks= new ArrayList<>();
+        for(int i=0; i<db; i++){
+            tasks.add(new Task(data[i][0], data[i][1], data[i][2], data[i][3]));
+        }
+
+        Scheduler sch =new Scheduler();
+        sch.order(tasks, db);
+
+
+        System.out.println(sch.list);
+        for(int i=0; i<db; i++) {
+            System.out.print(sch.name[i]+":");
+            System.out.print(sch.wait[i]);
+            if (i < db - 1) {
+                System.out.print(",");
+            }
+        }
+    }
 }
